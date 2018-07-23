1c3954c84d33a91c9a49ccd76637814cc5f00b2a
cdeocampo2001 <sweetcaroline601@gmail.com>
2018-07-23 22:11:14 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,17 @@
 # Code your prompts here!
 
 # Try starting out with puts'ing a string.
+guest_name = gets.strip
+party_name = gets.strip
+date = gets.strip
+time = gets.strip
+host_name = gets.strip
+rsvp_time = gets.strip
+
+puts "Dear #{guest_name},
+
+You are cordially invited to the Best Halloween Party Ever on #{date} at #{time}. Please RSVP no later than #{rsvp time}. 
+
+Sincerely, 
+
+#{host_name}''
